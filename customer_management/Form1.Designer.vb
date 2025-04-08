66db94372fb1296b7e19ccbdd6333587d471248b
anh nguyen <hoang.anh@key-link.jp>
2025-04-08 09:50:36 +0700

    updated view

--- customer_management/Form1.Designer.vb
+++ customer_management/Form1.Designer.vb
@@ -22,26 +22,24 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ListView1 = New System.Windows.Forms.ListView()
         Me.labelCustomerName = New System.Windows.Forms.Label()
         Me.txtCustomerName = New System.Windows.Forms.TextBox()
         Me.labelAddress = New System.Windows.Forms.Label()
         Me.txtAddress = New System.Windows.Forms.TextBox()
         Me.labelPhone = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
         Me.labelEmail = New System.Windows.Forms.Label()
         Me.txtEmail = New System.Windows.Forms.TextBox()
         Me.labelGender = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.comboGender = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.columName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnAddress = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnPhone = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnEmail = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.columnGender = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'ListView1
-        '
-        Me.ListView1.Location = New System.Drawing.Point(12, 155)
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(590, 252)
-        Me.ListView1.TabIndex = 0
-        Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'labelCustomerName
         '
@@ -84,12 +82,12 @@ Partial Class Form1
         Me.labelPhone.TabIndex = 5
         Me.labelPhone.Text = "Số điện thoại"
         '
-        'TextBox1
+        'txtPhone
         '
-        Me.TextBox1.Location = New System.Drawing.Point(441, 39)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(151, 20)
-        Me.TextBox1.TabIndex = 6
+        Me.txtPhone.Location = New System.Drawing.Point(441, 39)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(151, 20)
+        Me.txtPhone.TabIndex = 6
         '
         'labelEmail
         '
@@ -116,47 +114,98 @@ Partial Class Form1
         Me.labelGender.TabIndex = 9
         Me.labelGender.Text = "Giới tính"
         '
-        'ComboBox1
+        'comboGender
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Nam", "Nữ"})
-        Me.ComboBox1.Location = New System.Drawing.Point(229, 110)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(151, 21)
-        Me.ComboBox1.TabIndex = 10
+        Me.comboGender.FormattingEnabled = True
+        Me.comboGender.Items.AddRange(New Object() {"Nam", "Nữ"})
+        Me.comboGender.Location = New System.Drawing.Point(229, 110)
+        Me.comboGender.Name = "comboGender"
+        Me.comboGender.Size = New System.Drawing.Size(151, 21)
+        Me.comboGender.TabIndex = 10
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.columName, Me.columnAddress, Me.columnPhone, Me.columnEmail, Me.columnGender})
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 168)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(579, 239)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'columName
+        '
+        Me.columName.HeaderText = "Tên"
+        Me.columName.Name = "columName"
+        Me.columName.ReadOnly = True
+        '
+        'columnAddress
+        '
+        Me.columnAddress.HeaderText = "Địa chỉ"
+        Me.columnAddress.Name = "columnAddress"
+        Me.columnAddress.ReadOnly = True
+        Me.columnAddress.Width = 150
+        '
+        'columnPhone
+        '
+        Me.columnPhone.HeaderText = "Số điện thoại"
+        Me.columnPhone.Name = "columnPhone"
+        Me.columnPhone.ReadOnly = True
+        '
+        'columnEmail
+        '
+        Me.columnEmail.HeaderText = "Email"
+        Me.columnEmail.Name = "columnEmail"
+        Me.columnEmail.ReadOnly = True
+        '
+        'columnGender
+        '
+        Me.columnGender.HeaderText = "Giới tính"
+        Me.columnGender.Name = "columnGender"
+        Me.columnGender.ReadOnly = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(614, 419)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.ClientSize = New System.Drawing.Size(604, 419)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.comboGender)
         Me.Controls.Add(Me.labelGender)
         Me.Controls.Add(Me.txtEmail)
         Me.Controls.Add(Me.labelEmail)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtPhone)
         Me.Controls.Add(Me.labelPhone)
         Me.Controls.Add(Me.txtAddress)
         Me.Controls.Add(Me.labelAddress)
         Me.Controls.Add(Me.txtCustomerName)
         Me.Controls.Add(Me.labelCustomerName)
-        Me.Controls.Add(Me.ListView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ListView1 As System.Windows.Forms.ListView
     Friend WithEvents labelCustomerName As System.Windows.Forms.Label
     Friend WithEvents txtCustomerName As System.Windows.Forms.TextBox
     Friend WithEvents labelAddress As System.Windows.Forms.Label
     Friend WithEvents txtAddress As System.Windows.Forms.TextBox
     Friend WithEvents labelPhone As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
     Friend WithEvents labelEmail As System.Windows.Forms.Label
     Friend WithEvents txtEmail As System.Windows.Forms.TextBox
     Friend WithEvents labelGender As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents comboGender As System.Windows.Forms.ComboBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents columName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnAddress As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnPhone As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnEmail As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents columnGender As System.Windows.Forms.DataGridViewTextBoxColumn
 
 End Class
--- customer_management/Form1.vb
+++ customer_management/Form1.vb
@@ -1,10 +1,26 @@
 ﻿Public Class Form1
 
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles labelCustomerName.Click
+    Private Sub txtCustomerName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCustomerName.TextChanged
 
     End Sub
 
-    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+    Private Sub txtAddress_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtAddress.TextChanged
+
+    End Sub
+
+    Private Sub txtPhone_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPhone.TextChanged
+
+    End Sub
+
+    Private Sub txtEmail_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtEmail.TextChanged
+
+    End Sub
+
+    Private Sub comboGender_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles comboGender.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
 End Class
